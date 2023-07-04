e6661ae681d72cc6d43abf50e2966109bb6e3449
agenover <agenover@findmypast.com>
2023-07-04 22:10:33 +0200

    Added edit person logic and return to tree page

--- src/components/form/EditPerson.js
+++ src/components/form/EditPerson.js
@@ -1,42 +1,49 @@
 import React from "react";
 import PageLayout from "../layout/PageLayout";
 import FormBlank from "./formBlank";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 
 export default function EditPerson() {
     const location = useLocation();
+
     const data = location.state;
-    console.log({data})
+
+	const retrievePersonData = (personId) => {
+		const rawData = localStorage.getItem("site-data");
+		const currentData = JSON.parse(rawData);
+		const currentPerson = currentData.find(element => element.uniqueId === personId)
+		return currentPerson
+	}
+	
+	const addEditedPerson = (editedPerson) => {
+		const rawData = localStorage.getItem("site-data");
+		const currentData = JSON.parse(rawData);
+		const index = currentData.findIndex((element) => element.uniqueId === editedPerson.uniqueId);
+		currentData[index] = editedPerson;
+		localStorage.setItem("site-data", JSON.stringify(currentData));
+	};
+
+	const navigate = useNavigate();
+
+	const navigateToTree = () => {
+		navigate("/tree/")
+	}
+	
+	const onFormSubmit = (person) => {
+		addEditedPerson(person)
+		navigateToTree();
+	}
+	
+
 	return (
 		<PageLayout>
 			<div>
 				<h2>Edit person</h2>
-				<FormBlank initialValues={retrievePersonData(data)} onFormSubmit={onFormSubmit}/>
+				<FormBlank initialValues={retrievePersonData(data)} onFormSubmit={onFormSubmit} onCl/>
                 <div>{}</div>
 			</div>
 		</PageLayout>
 	);
 }
 
-const retrievePersonData = (personId) => {
-	const rawData = localStorage.getItem("site-data");
-    const currentData = JSON.parse(rawData);
-    const currentPerson = currentData.find(element => element.uniqueId === personId)
-
-    console.log({personId})
-    console.log({currentData})
-    console.log({currentPerson})
-    return currentPerson
-}
-
-const addEditedPerson = (person) => {
-	// const rawData = localStorage.getItem("site-data");
-	// const currentData = rawData ? JSON.parse(rawData) : [];
-	// currentData.push(person);
-	// localStorage.setItem("site-data", JSON.stringify(currentData));
-};
-
-const onFormSubmit = (person) => {
-	addEditedPerson(person)
-}
